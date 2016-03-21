4c2fcb297200a66d592074b0cd942fbe989dec18
hsmallbone <21306592@student.uwa.edu.au>
2016-03-21 19:10:18 +0800

    Initial Commit

---
+++ Bloom-MobileTests/Bloom_MobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bloom_MobileTests.swift
+//  Bloom-MobileTests
+//
+//  Created by Harry Smallbone on 21/03/2016.
+//  Copyright © 2016 Bloom. All rights reserved.
+//
+
+import XCTest
+@testable import Bloom_Mobile
+
+class Bloom_MobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
