f079d5f5bd53d7a6c0086fa395b3a7c98f304452
CodeOwl94 <ashwin@bloom.org.au>
2016-03-23 18:50:52 +0800

    Separate storyboards for profile and events

--- Bloom-Mobile/ProfileViewController.swift
+++ Bloom-Mobile/ProfileViewController.swift
@@ -1,25 +1,24 @@
-//  
-//  SecondViewController.swift
+//
+//  ProfileViewController.swift
 //  Bloom-Mobile
 //
-//  Created by Harry Smallbone on 21/03/2016.
+//  Created by Ashwin Daniel D'Cruz on 23/03/2016.
 //  Copyright © 2016 Bloom. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
