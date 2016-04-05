f31da5ed8d2a6c6b26b7184f9aabc54eef30cfb5
CodeOwl94 <ashwin@bloom.org.au>
2016-04-05 22:33:38 +0800

    Events boilerplate code

---
+++ Bloom-Mobile/Event.swift
@@ -0,0 +1,44 @@
+//
+//  Event.swift
+//  Bloom-Mobile
+//
+//  Created by Ashwin Daniel D'Cruz on 4/04/2016.
+//  Copyright © 2016 Bloom. All rights reserved.
+//
+
+import UIKit
+
+class Event: NSObject {
+    
+    //MARK: Properties
+    //These are based on some of the fields of an event in terms of Google's resource representation
+    var summary: String!
+    var eventDescription: String?
+    var location: String?
+    var startTime: String?
+    var endTime: String?
+    
+    
+    //MARK: Initialization
+    
+    init?(summary:String!, eventDescription:String?, location:String?, startTime:String?, endTime:String?) {
+
+        // Initialize stored properties.
+        self.summary = summary
+        self.eventDescription = eventDescription
+        self.location = location
+        self.startTime = startTime
+        self.endTime = endTime
+        
+        super.init()
+        
+        if(summary.isEmpty){
+            return nil
+        }
+        
+    }
+    
+    
+    
+    
+}
--- Bloom-Mobile/EventsViewController.swift
+++ Bloom-Mobile/EventsViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UITableViewController {
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
