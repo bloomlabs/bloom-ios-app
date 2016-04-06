604945ad03863764d32614f6c629547c399898fc
hsmallbone <21306592@student.uwa.edu.au>
2016-04-06 17:18:54 +0800

    Some work on rails API calls

--- Bloom-Mobile/BloomLoginViewController.swift
+++ Bloom-Mobile/BloomLoginViewController.swift
@@ -19,49 +19,53 @@ class BloomLoginViewController: UIViewController, GIDSignInUIDelegate {
         // Automatically sign in the user.
         GIDSignIn.sharedInstance().signInSilently()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func onGoogleLogin(signIn: GIDSignIn!, user: GIDGoogleUser!) {
-        let url = NSURL(string: "http://apply.bloom.org.au/api/user_auth_token")
-        let req = NSMutableURLRequest(URL: url!, cachePolicy: .UseProtocolCachePolicy, timeoutInterval: 60)
-        req.HTTPMethod = "POST"
+        self.activityIndicator.startAnimating()
         let data:NSDictionary = ["id_token" : user.authentication.idToken, "audience": user.authentication.clientID]
-        req.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        req.addValue("application/json", forHTTPHeaderField: "Accept")
-        req.addValue("Token token=93b36e75142e9eb0d33ed9ef5b6f19da59482bf82d646e451cf5dc27f9c1d14b91ce8043369ebe925ca9177c32624c1452eb9e291610a59b1b6bb66d2d433bce", forHTTPHeaderField: "Authorization")
-        req.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(data, options: [])
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(req) {(data, response, error) in
+        API.postJSON("http://apply.bloom.org.au/api/user_auth_token", data: data,  completionHandler: {(data, response, error) in
             if error != nil {
                 print(error)
                 return
             }
             let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: [])
-            if (json["error"] != nil) {
+            if json["error"] != nil {
                 print(json["error"])
                 return
             }
-            let token = json["token"]
+            let token:String = json["token"]
+            let user_id:String = json["id"]
+            API.postJSON("http://apply.bloom.org.au/api/profiles/" + user_id, data: nil, completionHandler: {(data, response, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: [])
+                if json["error"] != nil {
+                    print(json["error"])
+                    return
+                }
+            })
             self.activityIndicator.stopAnimating()
-        }
+        })
         
-        self.activityIndicator.startAnimating()
-        task.resume()
         
         self.performSegueWithIdentifier("loggedin", sender: self)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
