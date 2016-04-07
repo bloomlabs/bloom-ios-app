0c40d8b09079da83643aef7d40d4acd0a7527ed3
CodeOwl94 <ashwin@bloom.org.au>
2016-04-07 21:53:45 +0800

    Fixed some event storboard issues

--- Bloom-Mobile-Bridging-Header.h
+++ Bloom-Mobile-Bridging-Header.h
@@ -2,3 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import <Google/SignIn.h>
+#import "GTL/GTMOAuth2ViewControllerTouch.h"
+#import "GTLCalendar.h"
