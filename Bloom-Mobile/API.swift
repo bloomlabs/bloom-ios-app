6dcff4e4732109e98742cc54d28a7dd1ad93ccfc
hsmallbone <21306592@student.uwa.edu.au>
2016-04-13 16:59:54 +0800

    Move Bloom API key to environment file

--- Bloom-Mobile/API.swift
+++ Bloom-Mobile/API.swift
@@ -22,7 +22,8 @@ class API {
         req.HTTPMethod = "POST"
         req.addValue("application/json", forHTTPHeaderField: "Content-Type")
         req.addValue("application/json", forHTTPHeaderField: "Accept")
-        req.addValue("Token token=93b36e75142e9eb0d33ed9ef5b6f19da59482bf82d646e451cf5dc27f9c1d14b91ce8043369ebe925ca9177c32624c1452eb9e291610a59b1b6bb66d2d433bce", forHTTPHeaderField: "Authorization")
+        
+        req.addValue("Token token=" + Environment.getEnv("BloomAPIKey"), forHTTPHeaderField: "Authorization")
         req.HTTPBody = try! NSJSONSerialization.dataWithJSONObject(data, options: [])
         let task = NSURLSession.sharedSession().dataTaskWithRequest(req, completionHandler: completionHandler)
         task.resume()
